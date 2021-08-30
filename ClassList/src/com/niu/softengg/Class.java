f8106fd65a8696e7e3b910a0cd9a05572b5f1e95
Shalini <shalini@dileeps-MBP.wowway.com>
2021-08-30 00:35:40 -0500

    main method print

--- ClassList/src/com/niu/softengg/Class.java
+++ ClassList/src/com/niu/softengg/Class.java
@@ -3,7 +3,7 @@ package com.niu.softengg;
 public class Class {
 
 	public static void main(String[] args) {
-		System.out.print("ClassList Class main ");
+		System.out.print("ClassList Class main function called");
 	}
 	
 }
